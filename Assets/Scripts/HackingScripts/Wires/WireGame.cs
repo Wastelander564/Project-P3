a747de57da89a976b8fcca4dbde340dcbb18d7e9
yup50 <deronyouri@gmail.com>
2025-04-09 11:46:12 +0200

    work

---
+++ Assets/Scripts/HackingScripts/Wires/WireGame.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class WireGame : MiniGame
+
+{
+    public Wire currentDrag;
+    public Wire Hoverd;
+
+    private int correctWires;
+
+
+    private void Enable()
+    {
+        lives = 3;
+        currentDrag = null;
+        Hoverd = null;
+    }
+    public void CorrectWires()
+    {
+        correctWires++;
+    }
+
+    private void Update()
+    {
+        if(correctWires == 4)
+        {
+            Win();
+        }
+    }
+}
